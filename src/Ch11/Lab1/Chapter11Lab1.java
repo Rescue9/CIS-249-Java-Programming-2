80bf0c904e194292bf6d35438f59614af915f935
Rescue9 <Rescue9@yahoo.com>
2013-01-17 15:41:47 -0600

    working on Ch11Lab1. errors with duplicate method though

---
+++ src/Ch11/Lab1/Chapter11Lab1.java
@@ -0,0 +1,62 @@
+/*
+ * Program: Chapter11Lab1.java
+ * Programmer: Andrew Buskov
+ * Class: CIS 249
+ * Date: Jan 17, 2013
+ * Purpose: This program will request the size of the sides of the triangle and determine the area and perimeter
+ * it will also ask if the user wants a fill color. If no fill is designated it will display that the 
+ * triangle will not be solid.
+ */
+
+package Ch11.Lab1;
+
+import java.util.Scanner;
+
+public class Chapter11Lab1 {
+	
+	public static void main(String[] args){
+		//declare the variable, Scanner object and set repeat to yes
+		Scanner input = new Scanner(System.in);
+		double[] sides = new double[3];
+		String color;
+		boolean filled = false;
+		String repeat = "yes";
+	
+		// create do/while loop that will run as long as repeat = yes
+		do {
+			System.out.println("Enter three sides; ");
+			for (int i=0; i<sides.length; i++){
+			sides[i] = input.nextDouble();
+			}
+			
+			System.out.println("Enter the color: ");
+			color = input.next();
+		
+			Triangle triangle =  new Triangle(sides[0], sides[1], sides[2], color);
+		
+			System.out.println("Enter true if you want to fill the triangle with the color: " + color + ". If you " +
+				"do not want to fill the triangle with color please enter false.");
+		
+			filled = input.nextBoolean();
+		
+			if (filled == false){
+				color = " No color";
+				triangle.setColor(color);
+			}
+		
+			triangle.setFilled(filled);
+		
+		
+			// output information
+			System.out.println();
+			System.out.println("The area is " + triangle.getArea());
+			System.out.println("The perimeter is " + triangle.getPerimeter());
+			System.out.println("The color is " + triangle.getColor());
+			System.out.println(triangle);
+			System.out.println();
+			
+			System.out.println("Do you wish to enter new values? Please answer yes/no");
+			repeat = input.next();
+		} while (repeat.equalsIgnoreCase("yes"));
+	}
+}
---
+++ src/Ch11/Lab1/Triangle.java
@@ -0,0 +1,75 @@
+/*
+ * Program: Triangle.java
+ * Programmer: Andrew Buskov
+ * Class: CIS 249
+ * Date: Jan 17, 2013
+ * Purpose: A class that extends GeometricObject
+ */
+
+package Ch11.Lab1;
+
+public class Triangle extends GeometricObject {
+	
+	// declare variables for the sides of the triangle and its color
+	private double side1, side2, side3;
+	private String color;
+	
+	/** Default Constructor */
+	public Triangle() {
+		side1 = 0.0;
+		side2 = 0.0;
+		side3 = 0.0;
+		color = "No color";
+	}
+	
+	/** Constructor */
+	public Triangle(double side1, double side2, double side3, String color) {
+		this.side1 = side1;
+		this.side2 = side2;
+		this.side3 = side3;
+		this.color = color;
+	}
+	
+	public double getSide1(){
+		return side1;
+	}
+	
+	public double getSide2(){
+		return side2;
+	}
+	
+	public double getSide3(){
+		return side3;
+	}
+	
+	
+	/** Override method findArea in GeometricObject */
+	@Override
+	public double getArea(){
+		double s = (side1 + side2 + side3) / 2;
+		return Math.sqrt(s * (s - side1) * (s - side2) * (s - side3));
+	}
+	
+	public String getColor(){
+		return color;
+	}
+	
+	/** Implement the abstract method findArea in GeometricObject */
+	public double getArea(){
+		double s = (side1 + side2 + side3) / 2;
+		return Math.sqrt(s * (s - side1) * (s - side2) * (s - side3));
+	}
+	
+	/** Implement the abstract method findCircumference in
+	 * GeometricObject
+	 */
+	public double getPerimeter(){
+		return side1 + side2 + side3;
+	}
+	
+	public String toString(){
+		// Implement it to return the three sides and the color
+		return "Triangle side1 = " + side1 + " side2 = " + side2 + " side3 = " + side3 + " color = " + color;
+	}
+
+}
