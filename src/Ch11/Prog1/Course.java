c07899aca13ca7fe48436d8a53a76e45dc96dd92
Rescue <Rescue@corridor9.net>
2013-01-24 17:58:23 -0600

    started course.java

---
+++ src/Ch11/Prog1/Course.java
@@ -0,0 +1,34 @@
+package Ch11.Prog1;
+
+public class Course {
+	
+	private String courseName;
+	private String[] students = new String[100];
+	private int numberOfStudents;
+	
+	public Course(String courseName){
+		this.courseName = courseName;
+	}
+	
+	public void addStudent(String student){
+		students[numberOfStudents] = student;
+		numberOfStudents++;
+	}
+	
+	public String[] getStudents(){
+		return students;
+	}
+	
+	public int getNumberOfStudents(){
+		return numberOfStudents;
+	}
+	
+	public String getCourseName(){
+		return courseName;
+	}
+	
+	public void dropStudent(String student){
+		// left as exercise
+	}
+
+}
