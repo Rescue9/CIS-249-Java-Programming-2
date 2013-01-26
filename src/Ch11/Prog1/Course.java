64c09e93ad8fafa2b43b67a1a8b1b1eb4aa09c96
Rescue <Rescue@corridor9.net>
2013-01-26 16:37:27 -0600

    altered eclipse preferences finished Ch11Prog1 created work area for testing

--- src/Ch11/Prog1/Course.java
+++ src/Ch11/Prog1/Course.java
@@ -1,34 +1,50 @@
+/**
+ * Program: Course.java
+ * Programmer: Andrew Buskov
+ * Class: CIS 249
+ * Date: Jan 19, 2013
+ * Purpose: (The Course class) Rewrite the Course class in Listing 10.6. Use an ArrayList
+ * to replace an array to store students. You should not change the original contract of
+ * the Course class (i.e., the definition of the constructors and methods should not
+ * be changed).
+ */
+
 package Ch11.Prog1;
 
+import java.util.ArrayList;
+
 public class Course {
-	
-	private String courseName;
-	private String[] students = new String[100];
-	private int numberOfStudents;
-	
-	public Course(String courseName){
-		this.courseName = courseName;
+	private ArrayList<String> courseName = new ArrayList<>();
+	private ArrayList<String> students = new ArrayList<>();
+
+	public Course(){	
 	}
 	
-	public void addStudent(String student){
-		students[numberOfStudents] = student;
-		numberOfStudents++;
-	}
-	
-	public String[] getStudents(){
-		return students;
-	}
-	
-	public int getNumberOfStudents(){
-		return numberOfStudents;
-	}
-	
-	public String getCourseName(){
-		return courseName;
-	}
-	
-	public void dropStudent(String student){
-		// left as exercise
+	public Course(String myCourseName) {
+		courseName.add(myCourseName);
 	}
 
+	public void addStudent(String student) {
+		students.add(student);
+	}
+
+	public String getStudents(int num) {
+		return students.get(num);
+	}
+
+	public int getNumberOfStudents() {
+		return students.size()-1;
+	}
+
+	public void CourseName(String newCourseName) {
+		courseName.add(newCourseName);
+	}
+	
+	public void getCourseName(int num) {
+		System.out.println(courseName.get(num));
+	}
+
+	public void dropStudent(String student) {
+		students.remove(student);
+	}
 }
