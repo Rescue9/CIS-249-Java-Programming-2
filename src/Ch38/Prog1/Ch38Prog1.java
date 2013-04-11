bc194491959d9226de838941460a1eac5c6dfffc
Rescue9 <Rescue9@yahoo.com>
2013-04-11 12:48:39 -0500

    Ch38Prog1 finished Ch38Prog2 GUI working

--- src/Ch38/Prog1/Ch38Prog1.java
+++ src/Ch38/Prog1/Ch38Prog1.java
@@ -13,16 +13,22 @@ import java.awt.Color;
 import java.awt.GridLayout;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.text.DecimalFormat;
 
 import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
+import javax.swing.JMenu;
+import javax.swing.JMenuBar;
+import javax.swing.JMenuItem;
 import javax.swing.JOptionPane;
 import javax.swing.JPanel;
 import javax.swing.JTextField;
 import javax.swing.border.LineBorder;
 
 public class Ch38Prog1 extends JFrame implements ActionListener {
+	
+	private static final long serialVersionUID = 1L; // eclipse complains
 	
 	// declare items
 	JLabel jlblInvAmount = new JLabel("Investment Amount");
@@ -35,8 +41,13 @@ public class Ch38Prog1 extends JFrame implements ActionListener {
 	JTextField jtfAnnIntRate = new JTextField();
 	JLabel jlblFutValueAns = new JLabel();
 	JButton	calcButton = new JButton("Calculate");
+	
+	DecimalFormat df = new DecimalFormat("$###,###,###.##");
 
 	public Ch38Prog1() {
+		
+		// set listener of calcButton
+		calcButton.addActionListener(this);
 		
 		// set border of investment area answer label
 		jlblFutValueAns.setBorder(new LineBorder(Color.gray, 1));
@@ -60,12 +71,14 @@ public class Ch38Prog1 extends JFrame implements ActionListener {
 		add(p, BorderLayout.CENTER);
 		add(pButton, BorderLayout.SOUTH);
 		
+		addMenuBar();
+		
 	}
 	
 	public double runCalculation(double invAmount, double years, double annIntRate){
 		double futureValue;
-		double monIntRate = annIntRate / 12;
-		futureValue = invAmount * Math.pow((1 + monIntRate), (years *12));
+		double monIntRate = annIntRate / 12 / 100;
+		futureValue = invAmount * Math.pow((1 + monIntRate), (years * 12));
 		return futureValue;
 	}
 	
@@ -82,19 +95,61 @@ public class Ch38Prog1 extends JFrame implements ActionListener {
 
 	@Override
 	public void actionPerformed(ActionEvent e) {
+		// menu section
+		if (e.getActionCommand().equals("Calculate")){
+			calcMethod();
+		}
+		if (e.getActionCommand().equals("Exit")){
+			System.exit(1);
+		}
+		if (e.getActionCommand().equals("About")){
+			JOptionPane.showMessageDialog(null, "Compute future investment value", "About This Program", JOptionPane.INFORMATION_MESSAGE);
+		}
+		
+		// calculation button
 		if(e.getSource() == calcButton){
-			if((jtfInvAmount !=null) && (jtfYears != null) && (jtfAnnIntRate != null)){
-				double invAmount = Double.parseDouble(jtfInvAmount.getText().trim());
-				double years = Double.parseDouble(jtfYears.getText().trim());
-				double anIntRate = Double.parseDouble(jtfAnnIntRate.getText().trim());
-				
-				runCalculation(invAmount, years, anIntRate);
-			} else {
-				JOptionPane.showMessageDialog(null, "alert", "alert", JOptionPane.ERROR_MESSAGE);
-			}
+			calcMethod();
 		}
 	}
 	
+	public void calcMethod(){
+		if((!"".equals(jtfInvAmount.getText())) && (!"".equals(jtfAnnIntRate.getText())) && (!"".equals(jtfYears.getText()))){
+			double invAmount = Double.parseDouble(jtfInvAmount.getText());
+			double years = Double.parseDouble(jtfYears.getText());
+			double anIntRate = Double.parseDouble(jtfAnnIntRate.getText());
+			
+			double futureValueAnswer = runCalculation(invAmount, years, anIntRate);
+			jlblFutValueAns.setText(""+df.format(futureValueAnswer));
+		} else {
+				JOptionPane.showMessageDialog(null, "None of the fields can be empty!", "ERROR", JOptionPane.ERROR_MESSAGE);
+		}
+	}
 	
-
+	public void addMenuBar(){
+		JMenuBar mb = new JMenuBar();
+		this.setJMenuBar(mb);
+		
+		JMenu operation = new JMenu("Operation");
+		operation.setMnemonic('o');
+		mb.add(operation);
+		
+		JMenu help = new JMenu("Help");
+		help.setMnemonic('h');
+		mb.add(help);
+		
+		JMenuItem calculate = new JMenuItem("Calculate");
+		calculate.setMnemonic('c');
+		calculate.addActionListener(this);
+		operation.add(calculate);
+		
+		JMenuItem exit = new JMenuItem("Exit");
+		exit.setMnemonic('x');
+		exit.addActionListener(this);
+		operation.add(exit);
+		
+		JMenuItem about = new JMenuItem("About");
+		about.setMnemonic('a');
+		about.addActionListener(this);
+		help.add(about);
+	}
 }
---
+++ src/Ch38/Prog2/Ch38Prog2.java
@@ -0,0 +1,122 @@
+/**
+ * Program: Ch38Prog2.java
+ * Programmer: Andrew Buskov
+ * Class: CIS 249
+ * Date: Apr 11, 2013
+ * Purpose: To display inputed text in a selected color
+ */
+
+package Ch38.Prog2;
+
+import java.awt.BorderLayout;
+import java.awt.Color;
+import java.awt.GridLayout;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.JTextField;
+
+public class Ch38Prog2 extends JFrame implements ActionListener{
+	// declare graphical items
+	JButton blue = new JButton("Blue");
+	JButton black = new JButton("Black");
+	JButton darkGray = new JButton("Dark Gray");
+	JButton gray = new JButton("Gray");
+	JButton red = new JButton("Red");
+	JButton magenta = new JButton("Magenta");
+	
+	JTextField inputArea= new JTextField();
+
+	public Ch38Prog2() {
+		// create textarea panel
+		JPanel textPanel = new JPanel();
+		textPanel.setLayout(new GridLayout(1,1));
+		textPanel.add(inputArea);
+		
+		// button panel
+		JPanel buttonPanel = new JPanel();
+		buttonPanel.setLayout(new GridLayout(3,2));
+		buttonPanel.add(blue);
+		buttonPanel.add(black);
+		buttonPanel.add(darkGray);
+		buttonPanel.add(gray);
+		buttonPanel.add(red);
+		buttonPanel.add(magenta);
+		
+		this.setLayout(new BorderLayout());
+		this.add(textPanel, BorderLayout.NORTH);
+		this.add(buttonPanel, BorderLayout.CENTER);
+		
+		createButtons();
+	}
+	
+	public static void main(String[] args){
+		Ch38Prog2 frame = new Ch38Prog2();
+		frame.setName("Chapter 38 Program 2");
+		frame.setSize(400, 300);
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frame.setLocationRelativeTo(null);
+		frame.setVisible(true);
+	}
+	
+	public void createButtons(){
+		// button action listeners
+		blue.addActionListener(this);
+		black.addActionListener(this);
+		darkGray.addActionListener(this);
+		gray.addActionListener(this);
+		red.addActionListener(this);
+		magenta.addActionListener(this);
+		
+		// set color properties
+		blue.setBackground(Color.blue);
+		blue.setForeground(Color.white);
+		black.setBackground(Color.black);
+		black.setForeground(Color.white);
+		darkGray.setBackground(Color.darkGray);
+		darkGray.setForeground(Color.white);
+		gray.setBackground(Color.gray);
+		gray.setForeground(Color.white);
+		red.setBackground(Color.red);
+		red.setForeground(Color.white);
+		magenta.setBackground(Color.magenta);
+		magenta.setForeground(Color.white);
+	}
+
+	@Override
+	public void actionPerformed(ActionEvent e) {
+		if (e.getSource().equals(blue)){
+			changeText(Color.blue);
+		}
+		if (e.getSource().equals(black)){
+			changeText(Color.black);
+		}
+		if (e.getSource().equals(darkGray)){
+			changeText(Color.darkGray);
+		}
+		if (e.getSource().equals(gray)){
+			changeText(Color.gray);
+		}
+		if (e.getSource().equals(red)){
+			changeText(Color.red);
+		}
+		if (e.getSource().equals(magenta)){
+			changeText(Color.magenta);
+		}
+	}
+	
+	public void changeText(Color color){
+		inputArea.setForeground(Color.white);
+		inputArea.setBackground(color);
+		
+	}
+	
+	public void setDefaultText(){
+		inputArea.setForeground(Color.black);
+		inputArea.setBackground(Color.white);
+	}
+
+}
