1254107b93e63920adc537873695ce990cc904f1
Rescue9 <Rescue9@yahoo.com>
2013-04-11 13:29:39 -0500

    finished Ch38Prog2 added zips

--- src/Ch38/Prog2/Ch38Prog2.java
+++ src/Ch38/Prog2/Ch38Prog2.java
@@ -16,10 +16,17 @@ import java.awt.event.ActionListener;
 
 import javax.swing.JButton;
 import javax.swing.JFrame;
+import javax.swing.JMenu;
+import javax.swing.JMenuBar;
+import javax.swing.JMenuItem;
+import javax.swing.JOptionPane;
 import javax.swing.JPanel;
 import javax.swing.JTextField;
 
 public class Ch38Prog2 extends JFrame implements ActionListener{
+	
+	private static final long serialVersionUID = 1L; // eclipse complains
+	
 	// declare graphical items
 	JButton blue = new JButton("Blue");
 	JButton black = new JButton("Black");
@@ -29,6 +36,8 @@ public class Ch38Prog2 extends JFrame implements ActionListener{
 	JButton magenta = new JButton("Magenta");
 	
 	JTextField inputArea= new JTextField();
+	JMenuBar mb = new JMenuBar();
+
 
 	public Ch38Prog2() {
 		// create textarea panel
@@ -49,8 +58,10 @@ public class Ch38Prog2 extends JFrame implements ActionListener{
 		this.setLayout(new BorderLayout());
 		this.add(textPanel, BorderLayout.NORTH);
 		this.add(buttonPanel, BorderLayout.CENTER);
+		this.setJMenuBar(mb);
 		
 		createButtons();
+		createMenu();
 	}
 	
 	public static void main(String[] args){
@@ -85,9 +96,45 @@ public class Ch38Prog2 extends JFrame implements ActionListener{
 		magenta.setBackground(Color.magenta);
 		magenta.setForeground(Color.white);
 	}
+	
+	public void createMenu(){
+		
+		JMenu file = new JMenu("File");
+		file.setMnemonic('f');
+		mb.add(file);
+		
+		JMenu edit = new JMenu("Edit");
+		edit.setMnemonic('e');
+		mb.add(edit);
+		
+		JMenu about = new JMenu("About");
+		about.setMnemonic('a');
+		mb.add(about);
+		
+		JMenuItem display = new JMenuItem("Display");
+		display.setMnemonic('d');
+		display.addActionListener(this);
+		file.add(display);
+		
+		JMenuItem exit = new JMenuItem("Exit");
+		exit.setMnemonic('x');
+		exit.addActionListener(this);
+		file.add(exit);
+		
+		JMenuItem clear = new JMenuItem("Clear");
+		clear.setMnemonic('c');
+		clear.addActionListener(this);
+		edit.add(clear);
+		
+		JMenuItem aboutDesc = new JMenuItem("About Color Buttons");
+		aboutDesc.setMnemonic('b');
+		aboutDesc.addActionListener(this);
+		about.add(aboutDesc);
+	}
 
 	@Override
 	public void actionPerformed(ActionEvent e) {
+		// button actions
 		if (e.getSource().equals(blue)){
 			changeText(Color.blue);
 		}
@@ -106,6 +153,20 @@ public class Ch38Prog2 extends JFrame implements ActionListener{
 		if (e.getSource().equals(magenta)){
 			changeText(Color.magenta);
 		}
+		
+		// menu actions
+		if (e.getActionCommand().equals("Display")){
+			setDefaultText();
+		}
+		if (e.getActionCommand().equals("Exit")){
+			System.exit(1);
+		}
+		if (e.getActionCommand().equals("Clear")){
+			inputArea.setText("");
+			setDefaultText();
+		}
+		if (e.getActionCommand().equals("About Color Buttons")){
+			JOptionPane.showMessageDialog(null, "This program changes the color of the text area to the seleced button's color.", "About Color Buttons", JOptionPane.INFORMATION_MESSAGE);		}
 	}
 	
 	public void changeText(Color color){
