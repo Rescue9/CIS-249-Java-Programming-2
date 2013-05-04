f0b8b3b6f4761e3d77cdb87ba43d3a98b7588f59
Rescue9 <Rescue9@yahoo.com>
2013-05-03 22:16:45 -0500

    added welcome text... tired now

--- src/Final/CIS249Final.java
+++ src/Final/CIS249Final.java
@@ -10,23 +10,56 @@ package Final;
 
 
 import java.awt.BorderLayout;
+import java.awt.Font;
+import java.awt.GraphicsEnvironment;
+import java.awt.GridLayout;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.util.Arrays;
+import java.util.List;
 
 import javax.swing.*;
 
 public class CIS249Final extends JApplet {
 	
+	private static final long serialVersionUID = 1L; // eclipse complains
+	
 	private static final String WELCOME_TEXT = "Welcome to Racing at Churchill Downs";
+	private static final String DISPLAY_TEXT = "Choose the post position of the horse in Race 1, "
+			+ "enter bet in the lower box, and press 'Place a Bet'";
+	
+	private JLabel jlblWelcome = new JLabel();
 	
 	private JTextField jtfDisplay = new JTextField();
 	private JTextField jtfBet = new JTextField();
 	
-	private JButton jbtPlaceBet = new JButton("Place a bet");
+	private JButton jbtPlaceBet = new JButton("Place a Bet");
 	private JButton[] jbtPosition = new JButton[4]; 
 	
+	private JPanel panel[] = new JPanel[5];
+	
 	public CIS249Final() {
+		// set out layout
+		setLayout(new GridLayout(5,1));
+		
+		// add menubar to the app
 		addMenuBar();
+		
+		// create panels
+		createPanels();
+		
+		// add the welcome message & set the font
+		jlblWelcome.setText(WELCOME_TEXT);
+		panel[1].add(jlblWelcome);
+		jlblWelcome.setFont(new Font(determineFont(), Font.PLAIN, 20));
+		
+		// add the display textfield
+		panel[2].add(jtfDisplay);
+		
+		
+		for (int i = 0; i< panel.length; i++){
+		add(panel[i]);
+		}
 	}
 	
 	public static void main(String[] args){
@@ -43,7 +76,6 @@ public class CIS249Final extends JApplet {
 	}
 	
 	public void addMenuBar(){
-		// add a menubar to our frame
 		JMenuBar mb = new JMenuBar();
 		this.setJMenuBar(mb);
 		
@@ -74,7 +106,7 @@ public class CIS249Final extends JApplet {
 		// set actionListeners for menuItems
 		jmiDisplay.addActionListener(new ActionListener(){
 			public void actionPerformed(ActionEvent e){
-				//execDisplay();
+				jtfDisplay.setText(DISPLAY_TEXT);
 			}
 			
 		});
@@ -91,5 +123,28 @@ public class CIS249Final extends JApplet {
 				jtfDisplay.setText("");
 				System.out.println("Reset Program");			}
 		});
+	}
+	
+	public void createPanels(){
+		int i = 0;
+		while (i < panel.length){
+			panel[i] = new JPanel();
+			i++;
+		}
+	}
+	
+	public String determineFont(){
+		GraphicsEnvironment e = GraphicsEnvironment.getLocalGraphicsEnvironment();
+		String[] fontNames = e.getAvailableFontFamilyNames();
+		
+		// convert to list for easier use
+		List<String> fontList = Arrays.asList(fontNames);
+		//System.out.println(fontList);	// TODO used for testing
+		String useFont;
+		if (fontList.contains("Roboto Lt")){
+			useFont = "Roboto Lt";
+			return useFont;
+		} else useFont = "Sans Serif";
+		return useFont;
 	}
 }
